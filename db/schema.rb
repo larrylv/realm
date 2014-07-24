a70d920a5de5c5c6f7024f20297bbc2f2d56cae8
Larry Lv <larrylv1990@gmail.com>
2014-07-24 17:26:50 +0800

    Add province model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140724092440) do
+
+  create_table "provinces", force: true do |t|
+    t.string  "name"
+    t.integer "population"
+  end
 
 end
