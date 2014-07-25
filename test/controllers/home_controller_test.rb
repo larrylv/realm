484eff1945580a76d39b28d58729de4417c58b3b
Larry Lv <larrylv1990@gmail.com>
2014-07-25 15:17:48 +0800

    Finish index page.

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+  test "should set title" do
+    get :index
+    assert_select 'title', 'D3 examples by @larrylv'
+  end
 end
