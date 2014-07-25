9d2b785c361ea97a21a9e2b3b9afa91e88688f3c
Larry Lv <larrylv1990@gmail.com>
2014-07-25 15:05:52 +0800

    Add more tests for ChartsController.

--- test/controllers/charts_controller_test.rb
+++ test/controllers/charts_controller_test.rb
@@ -1,8 +1,19 @@
 require 'test_helper'
 
 class ChartsControllerTest < ActionController::TestCase
-  test "should get population" do
+  test "[population]: should get population" do
     get :population
     assert_response :success
+  end
+
+  test "[population]: should get provinces json data" do
+    @request.headers["Accept"] = "application/json"
+    get :population
+    assert_kind_of Array, JSON.parse(@response.body)
+  end
+
+  test "[population]: should set title" do
+    get :population
+    assert_select 'title', '中华人民共和国行政区人口表'
   end
 end
